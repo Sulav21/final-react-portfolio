50dbb87692eeadfdff134194935c20ee601a43a4
Sulav Adhikari <sulavadhikari17@gmail.com>
2022-03-30 11:06:27 +1100

    CV Added

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -2,8 +2,17 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {RiMessengerLine} from 'react-icons/ri'
-import {BsWhatsapp} from 'react-icons/bs'
+import emailjs from 'emailjs-com';
+import  { useRef } from 'react';
+
 export const Contact = () => {
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm('service_e270uf6', 'template_7oh8r2q', form.current, 'PD6aCV92gaP7efMe0')
+    e.target.reset()
+  };
+
   return (
     <section id="contact">
       <h5>Get In Touch</h5>
@@ -26,7 +35,7 @@ export const Contact = () => {
 
         </div>
 
-        <form action="">
+        <form ref={form} onSubmit={sendEmail}>
          <input type="text" name='name' placeholder='Your Full Name' required />
          <input type="email" name='email' placeholder='Your Email' required />
          <textarea name="message"rows="7" placeholder='Your message' required></textarea>
--- src/components/header/HeaderBtn.js
+++ src/components/header/HeaderBtn.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import CV from '../../assets/cv.pdf'
+import CV from '../../assets/Resume.pdf'
 
 export const HeaderBtn = () => {
   return (
     <div className="cta">
-        <a href='https://humornama.com/wp-content/uploads/2020/11/Kaisa-Laga-Mera-Mazak-meme-template-of-Sunil-Grover.jpg' className="btn" target='_blank'>Download CV</a>
+        <a href={CV} className="btn" target='_blank'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
   )
